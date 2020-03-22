c78e0969a88f10643595f62ddb92867a2683874a
Ozbolt Menegatti <ozbolt.menegatti@gmail.com>
2020-03-22 11:54:05 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function SkeLexonomy() {
+  console.log("not initialized");
+}
+
+SkeLexonomy.prototype.init = function() {
+  console.log("Works?");
+}
+
+module.exports = SkeLexonomy;
